1ce02aab4cb57d75de12ba515c89fa43fbac5e3c
msnr <msnr@msnrnomacbook-pro.local>
2017-01-22 17:29:57 +0900

    Initial Commit

---
+++ TouchBarPaintSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchBarPaintSample
+//
+//  Created by msnr on 2017/01/22.
+//  Copyright © 2017年 msnr. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
